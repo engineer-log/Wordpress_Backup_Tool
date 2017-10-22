f6674dcf1a76859e53665ba74dda03a1bffab085
Takeshi <t.fukuda@engineer-log.net>
2017-10-23 00:57:33 +0900

    識別子追加、日付修正

--- wordpress_backup_toos.sh
+++ wordpress_backup_toos.sh
@@ -1,36 +1,38 @@
 #!bin/sh
 
+#識別子  
+ID=$1
 #DBユーザー
-DB_USER=$1
+DB_USER=$2
 #DBパスワード
-DB_PASSWORD=$2
+DB_PASSWORD=$3
 #DB名
-DB_NAME=$3
+DB_NAME=$4
 #DBホスト
-DB_HOST=$4
+DB_HOST=$5
 #wordpressのファイルパス
-WORDPRESS_PATH=$5
+WORDPRESS_PATH=$6
 #バックアップ先
 BACKUP_PATH="/Backup"
 #バックアップ世代数
 NUMBER_OF_GENERATIONS="10"
 
-DATE=`date +%Y%m%d-%H%M%k`
+DATE=`date +%Y%m%d_%H-%M-%S`
 
 #フォルダ存分確認（ない場合は作成する）
-if [ ! -d $BACKUP_PATH/DB ]
+if [ ! -d $BACKUP_PATH/DB/"$ID" ]
 then
-    mkdir -m 777 $BACKUP_PATH/DB
+    mkdir -m 777 $BACKUP_PATH/DB/"$ID"
 fi
 
-if [ ! -d $BACKUP_PATH/FILE ]
+if [ ! -d $BACKUP_PATH/FILE/"$ID" ]
 then
-    mkdir -m 777 $BACKUP_PATH/FILE
+    mkdir -m 777 $BACKUP_PATH/FILE/"$ID"
 fi
 
 
 #DBのバックアップ
-eval "mysqldump -u$DB_USER -h$DB_HOST -p$DB_PASSWORD $DB_NAME | gzip > $BACKUP_PATH/DB/DB_$DATE.sql.gz"
+eval "mysqldump -u$DB_USER -h$DB_HOST -p$DB_PASSWORD $DB_NAME | gzip > $BACKUP_PATH/DB/$ID/DB_$DATE.sql.gz"
 if [ $? -ne 0 ]
 then
   echo "$DATE:DB Backup error." >> $BACKUP_PATH/Backup.log
@@ -38,7 +40,7 @@ then
 fi
 
 #ファイルバック
-eval tar czf $BACKUP_PATH/FILE/FILE_$DATE.tar.gz -C $WORDPRESS_PATH . --exclude ".git"
+eval tar czf $BACKUP_PATH/FILE/"$ID"/FILE_$DATE.tar.gz -C $WORDPRESS_PATH . --exclude ".git"
 if [ $? -ne 0 ]
 then
   echo "$DATE:Wordpress File Backup error." >> $BACKUP_PATH/Backup.log
@@ -63,5 +65,5 @@ function deletefile(){
 }
 
 #旧バックアップファイル削除
-deletefile "$BACKUP_PATH/FILE/FILE_"
-deletefile "$BACKUP_PATH/DB/DB_"
+deletefile "$BACKUP_PATH/FILE/$ID/FILE_"
+deletefile "$BACKUP_PATH/DB/$ID/DB_"
